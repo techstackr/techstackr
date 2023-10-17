23cb6918791dd641013f5a0cf2a130b076564123
prrrrreston <122579382+prrrrreston@users.noreply.github.com>
2023-10-17 18:50:17 -0500

    implemented popup

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/App.scss';
 import Nav from './containers/Nav';
 import FilterBox from './containers/FilterBox';
 import Feed from './containers/Feed';
+import Popup from './components/Popup';
 
 const App = () => {
+  const [isPopupVisible, setPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setPopupVisible(!isPopupVisible);
+  };
+
   return (
     <div id='app'>
       <div id='app-container'>
-        <Nav />
+        <Nav togglePopup={togglePopup} />
         <div id='main-container'>
           <FilterBox />
           <Feed />
         </div>
+          {isPopupVisible && <Popup />}
       </div>
     </div>
   );
