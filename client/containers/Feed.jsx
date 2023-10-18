2248afcc05d5ade7c108999a12c0255d74bfe9ee
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-10-18 12:02:19 -0400

    Render logos in filter box

--- client/containers/Feed.jsx
+++ client/containers/Feed.jsx
@@ -3,34 +3,16 @@ import './../styles/Feed.scss';
 import FeedItem from '../components/FeedItem.jsx';
 import FilterBox from './FilterBox';
 
-// test data
-// const posts = [
-//   {
-//     post_id: 1,
-//     user_id: 1,
-//     post_content: 'test post here',
-//     user_rating: 4,
-//     technology_id: 2,
-//   },
-//   {
-//     post_id: 2,
-//     user_id: 2,
-//     post_content: `here's another post`,
-//     user_rating: 5,
-//     technology_id: 4,
-//   },
-// ];
-
-const Feed = (props) => {
+const Feed = props => {
   // STATE HOOKS
   // const [feed, updateFeed] = useState(posts);
 
   const fetchFeed = () => {
     fetch('/posts')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .then(data => updateFeed(data))
-    .catch(error => console.log(error));
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(data => updateFeed(data))
+      .catch(error => console.log(error));
   };
 
   // fetch posts and update feed
@@ -39,21 +21,30 @@ const Feed = (props) => {
   // filter posts
   const handleFilter = event => {
     const filter = event.target.id;
-    const filteredFeed = feedItems.filter(feedItem => feedItem.technology_id === filter);
+    const filteredFeed = feedItems.filter(
+      feedItem => feedItem.technology_id === filter,
+    );
     updateFeed(filteredFeed);
   };
-console.log('here is feeds' + props.feed)
+  
+  console.log('here is feeds' + props.feed);
   const feedItems = props.feed.map((item, index) => {
-    console.log(item)
+    console.log(item);
     const date = new Date(item.post_timestamp);
     date.setHours(date.getHours() - 4);
     const readableDate = date.toLocaleString();
-    return <FeedItem key={`FeedItem-${index}`} item={item} readableDate = {readableDate} />;
+    return (
+      <FeedItem
+        key={`FeedItem-${index}`}
+        item={item}
+        readableDate={readableDate}
+      />
+    );
   });
 
   return (
     <div id='feed-container'>
-      <FilterBox handleFilter={handleFilter}/>
+      <FilterBox handleFilter={handleFilter} />
       {feedItems}
     </div>
   );
